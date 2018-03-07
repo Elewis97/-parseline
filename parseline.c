5f262e4af3ffbfa3c1e9fa3db684e1ead3755965
Eleanor Lewis <Elewis97@users.noreply.github.com>
2018-03-07 15:17:06 -0800

    Added previously written function to split strings

--- parseline.c
+++ parseline.c
@@ -5,6 +5,50 @@
 #define CMAX 512 /*Command line length max*/
 #define PMAX 10  /*Pipeline command max*/
 
+char** splitStr(char *path, const char delimiter)
+{
+	char **res = 0;
+	int count = 0;
+	char *tempPtr = path;
+	char *lastDelim = 0;
+	char delim[2];
+	int idx;
+	char *token;
+
+	/*init delim*/
+	delim[0] = delimiter;
+	delim[1] = 0;
+
+	/*get number of elements*/
+	while(*tempPtr) {
+		if(delimiter == *tempPtr) {
+			count ++;
+			lastDelim = tempPtr;
+		}
+		tempPtr ++;
+	}
+
+	count += lastDelim	 < (path + strlen(path) - 1);
+	count ++;
+
+	res = malloc(sizeof(char*) * count);
+
+	if (res) {
+		idx = 0;
+		token = strtok(path, delim);
+
+		while(token) {
+			assert(idx < count);
+			*(res + idx++) = strdup(token);
+			token = strtok(0, delim);
+		}
+		assert(idx == count - 1);
+		*(res + idx) = 0;
+	}
+	return res;
+}
+
+
 void getCommand()
 {
 	char command[CMAX];
